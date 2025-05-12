777c08850091d34cd10b2ac543b458c363650b08
farida2498 <125817327+faridaheydarova@users.noreply.github.com>
2025-05-12 21:27:34 +0400

    1. js giriş

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+let a=5;
+let b=2;
+
+//console.log( a+b);
+//string - sətir tipli bütün hər şey 
+
+//window.alert("F5 düyməsinə klik etməyin!"); //pop up
+
+function showMessage(){
+   window.alert("F5 düyməsinə klik etməyin!");
+    console.log("F5 düyməsinə klik etməyin!");
+    document.getElementById("btn").textContent="Salam!";
+       document.getElementById("img").src="search-2911.png"
+}
